e93a6ab98b53d797fcc01b66e2121b0142380b0d
Samyak <samyak@Samyaks-MacBook-Air.local>
2026-06-27 12:43:09 +0530

    Initial commit

--- frontend/src/api/propertyApi.js
+++ frontend/src/api/propertyApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://ai-property-price-calculator.onrender.com";
 
 export const predictPrice = async (payload) => {
   const response = await axios.post(
